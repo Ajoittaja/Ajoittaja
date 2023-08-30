a55541ed2fbfe50969adcc8eb64442033bb56397
Kasperi Kiviluoma <k.kiviluoma@pm.me>
2023-08-30 22:51:50 +0300

    Log ticks

--- src/main.c
+++ src/main.c
@@ -1,16 +1,23 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0a
+/**
+ * TODO: Add a description here
+ * Author: Kasperi Kiviluoma
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <nrfx_rtc.h>
 
-LOG_MODULE_REGISTER(Less4_Exer2, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(nRF52DK, LOG_LEVEL_DBG);
+
+const nrfx_rtc_t rtc = NRFX_RTC_INSTANCE(0);
 
 int main(void) {
-    printk("Hello World! %s\n", CONFIG_BOARD);
-    LOG_DBG("nRF Connect SDK Fundamentals");
+    nrfx_rtc_enable(&rtc);
+    nrfx_rtc_tick_enable(&rtc, true);
+
+    while (true) {
+        LOG_DBG("%u", nrfx_rtc_counter_get(&rtc));
+        k_msleep(1000);
+    }
     return 0;
 }
